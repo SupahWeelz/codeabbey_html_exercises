df00bc836ccfdc1556105cc5e343e86ab0c22bab
SupahWeelz <60757434+SupahWeelz@users.noreply.github.com>
2022-11-19 00:08:06 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-echo "Random value is " . rand();
+  var name = $_GET['name'];
+  var year = $_GET['year'];
+  var month = $_GET['month'];
+  var day = $_GET['day'];
+  var A = 32;
+  var B = 32;
+  var C = 16;
+  var lucky = (strlen(name) * A + month * B + day * C + year) % 31 + 1;
+  echo "Your lucky day is " . lucky;
 ?>
